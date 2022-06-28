f67174cb013fff8a480e7a49474b19c936e3fd06
david <contact@faztweb.com>
2022-06-27 23:25:03 -0500

    mapa integrado

--- src/app/rutas/rutas.page.ts
+++ src/app/rutas/rutas.page.ts
@@ -1,5 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 
+declare var google;
+
+interface Marker {
+  position: {
+    lat: number,
+    lng: number,
+  };
+  title: string;
+}
+
 @Component({
   selector: 'app-rutas',
   templateUrl: './rutas.page.html',
@@ -7,9 +17,44 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RutasPage implements OnInit {
 
+  map = null;
+
   constructor() { }
 
   ngOnInit() {
+    this.loadMap();
   }
 
+  loadMap() {
+    // create a new map by passing HTMLElement
+    const mapEle: HTMLElement = document.getElementById('map');
+    // create LatLng object
+    const myLatLng = { lat: -11.990324, lng: -77.015184 };
+    // create map
+    this.map = new google.maps.Map(mapEle, {
+      center: myLatLng,
+      zoom: 17
+    });
+
+    google.maps.event.addListenerOnce(this.map, 'idle', () => {
+      //this.renderMarkers();
+      mapEle.classList.add('show-map');
+      const marker = {
+        position: {
+          lat: -11.990324,
+          lng: -77.015184
+        },
+        title: 'punto uno'
+      };
+      this.addMarker(marker);
+    });
+  }
+
+  addMarker(marker: Marker) {
+    return new google.maps.Marker({
+      position: marker.position,
+      map: this.map,
+      title: marker.title
+    });
+  }
 }
